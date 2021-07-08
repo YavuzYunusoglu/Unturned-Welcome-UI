c73adb32906689a47db321913265beb4c71ce2f4
Yavuz Yunusoğlu <54793496+YavuzYunusoglu@users.noreply.github.com>
2021-07-08 23:10:43 +0300

    Add files via upload

---
+++ KK_Welcome_UI/KK_Welcome_UI/Config.cs
@@ -0,0 +1,22 @@
+﻿using Rocket.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KK_Welcome_UI
+{
+    public class Config : IRocketPluginConfiguration
+    {
+        public string SunucuIsmi { get; set; }
+        public string DiscordAciklama { get; set; }
+        public string DiscordLink { get; set; }
+        public void LoadDefaults()
+        {
+            DiscordAciklama = "Discorda Katil";
+            SunucuIsmi = "Kanli Kurt";
+            DiscordLink = "https://discord.gg/Z7aKCPZeSj";
+        }
+    }
+}
